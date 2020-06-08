793bfe046fc738ddb6429111835d2e4c5e77f1a2
xenoonn <43119566+xenoonn@users.noreply.github.com>
2020-06-09 01:22:35 +0300

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,5 @@
+TOKEN = '1025851546:AAFJxW3RS_orAFEFI9OY1Fg71t38MQ58ei4'
+PROXY = {
+    'https': 'socks5://251715068:tmk0LS8U@orbtl.s5.opennetwork.cc:999',
+    'socks5': 'socks5://251715068:tmk0LS8U@orbtl.s5.opennetwork.cc:999'
+}
